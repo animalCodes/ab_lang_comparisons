4dbbdf755bef926313b93349e98501845150dd1d
animalCodes <84966313+animalCodes@users.noreply.github.com>
2023-01-04 10:39:14 +0000

    Write runner for rust using cargo

---
+++ code/rust/runner.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Runner for all Rust programs.
+
+# Check cargo command exists
+if ! type cargo > /dev/null; then
+  echo "cargo not found, please install to run."
+  exit;
+fi
+
+# This CD's into the directory this script is in, I'm not going to pretend to understand how.
+cd "${BASH_SOURCE%/*}"
+
+# Get name of program from user, eg "hello_world"
+echo "Enter name of program to run:"
+read NAME
+
+# Check specified directory exists, if it does CD into it.
+if [ -d $NAME ]; then
+  cd $NAME
+else
+  echo "\"$NAME\" doesn't exist. (Hint: the 'name of program' is the name of the folder containing said program."
+  exit;
+fi
+
+# Check src/ exists before running
+if [ -d "src" ]; then
+  cargo run --target-dir ../out/$NAME
+else
+  echo "No src folder found, exiting"
+  exit;
+fi
