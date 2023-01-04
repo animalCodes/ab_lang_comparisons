f610c12565e4d72bf04ea087b4e447c887a5fffb
animalCodes <84966313+animalCodes@users.noreply.github.com>
2023-01-04 15:41:23 +0000

    Implement hello_world in Go

---
+++ code/go/hello_world/go.mod
@@ -0,0 +1,3 @@
+module go/hello_world
+
+go 1.19
---
+++ code/go/hello_world/main.go
@@ -0,0 +1,8 @@
+// Must be set to 'main' otherwise Go will freak out
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
