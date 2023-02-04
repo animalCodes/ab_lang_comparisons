4ae13d901bbc41f615e7ba3f3c29784923032b07
animalCodes <84966313+animalCodes@users.noreply.github.com>
2023-02-04 12:57:51 +0000

    cleanup fizzbuzz Go implementation - fix indentation and remove unnecessary parentheses from if statement

--- code/go/fizzbuzz/main.go
+++ code/go/fizzbuzz/main.go
@@ -7,31 +7,30 @@ import (
 )
 
 func main() {
-    if (len(os.Args) <= 1) {
+    if len(os.Args) <= 1 {
         fmt.Println("No number specified, exiting")
         os.Exit(1)
     }
-    
     // String conversion package - string to int
     // If we wanted to ignore the error, we would do `input, _ := ..`
-	input, err := strconv.Atoi(os.Args[1])
+        input, err := strconv.Atoi(os.Args[1])
 
     // Go uses `nil` instead of `null` because reasons.
-	if err != nil {
-		fmt.Println("An error occurred while converting input to a number: ", err)
+    if err != nil {
+        fmt.Println("An error occurred while converting input to a number: ", err)
         fmt.Println("Exiting.")
         os.Exit(2)
-	}
+    }
 
-	var output string = ""
-	if input%3 == 0 {
-		output += "Fizz"
-	}
-	if input%5 == 0 {
-		output += "Buzz"
-	}
-	if input == 0 || output == "" {
-		output = os.Args[1]
-	}
-	fmt.Println(output)
+    var output string = ""
+    if input % 3 == 0 {
+        output += "Fizz"
+    }
+    if input % 5 == 0 {
+        output += "Buzz"
+    }
+    if input == 0 || output == "" {
+        output = os.Args[1]
+    }
+    fmt.Println(output)
 }
