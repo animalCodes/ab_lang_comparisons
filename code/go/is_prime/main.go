95ec69092d053bf62cce9efd2324694b6191136a
animalCodes <84966313+animalCodes@users.noreply.github.com>
2023-04-12 17:23:38 +0100

    Update is_prime Go implementation.

--- code/go/is_prime/main.go
+++ code/go/is_prime/main.go
@@ -20,16 +20,14 @@ func main() {
         os.Exit(2)
     }
 
-    // Exit early if num < 2
-    if num < 2 {
+    // Exit early if num can't be prime
+    if num < 2 || num > 2 && num % 2 == 0 || num > 3 && num % 3 == 0 {
         fmt.Println("False")
         os.Exit(0)
     }
 
-    comp := num-1 // "COMPare"
-    // Go only has a `for` loop construct, but it can double as a while loop.
-    for ; comp > 1; comp-- {
-        if (num % comp == 0) {
+    for i := 5; i*i < num; i += 6 {
+        if num % i == 0 || num % i+2 == 0 {
             fmt.Println("False")
             os.Exit(0)
         }
