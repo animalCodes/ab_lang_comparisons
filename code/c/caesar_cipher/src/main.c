417b138860e7bbb0ebef89d8875bff3ea7a87491
animalCodes <84966313+animalCodes@users.noreply.github.com>
2024-07-17 22:17:00 +0100

    Consistently use 4 spaces for indentation in all c implementations

--- code/c/caesar_cipher/src/main.c
+++ code/c/caesar_cipher/src/main.c
@@ -11,67 +11,69 @@ char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 // none exists.
 int indexOf(char needle, char *haystack) 
 {
-  // I don't know if I'm being dumb but I couldn't find an equivalent in the standard library.
-  for (int i = 0; i < strlen(haystack); i++) {
-    if (haystack[i] == needle)
-      return i;
-  }
-  return -1;
+    // I don't know if I'm being dumb but I couldn't find an equivalent in the
+    // standard library.
+    for (int i = 0; i < strlen(haystack); i++) {
+        if (haystack[i] == needle)
+            return i;
+    }
+    return -1;
 }
 
 // Returns `original_char` shifted `shiftc` (Shift Count) places up or down
 // the alphabet.
 char shift(char original_char, int shiftc)
 {
-  // Repeatedly calling strlen() caused sanity to cease, I'm talking 26 + -3 =
-  // 19. I don't know why, and I don't know why this fixes it, but it does.
-  int alphabet_length = strlen(alphabet);
+    // Repeatedly calling strlen() caused sanity to cease, I'm talking 26 + -3 =
+    // 19. I don't know why, and I don't know why this fixes it, but it does.
+    int alphabet_length = strlen(alphabet);
 
-  int index = indexOf(original_char, alphabet);
-  if (index == -1)
-    // Don't try to shift none-alphabetical characters
-    return original_char;
+    int index = indexOf(original_char, alphabet);
+    if (index == -1)
+        // Don't try to shift none-alphabetical characters
+        return original_char;
 
-  index += shiftc;
+    index += shiftc;
 
-  // Avoid overflow, but allow wrap-around shifts.
-  while (index >= alphabet_length)
-    index -= alphabet_length;
+    // Avoid overflow, but allow wrap-around shifts.
+    while (index >= alphabet_length)
+        index -= alphabet_length;
 
-  // Same as above, but for negative shifts.
-  while (index < 0)
-    index += alphabet_length;
+    // Same as above, but for negative shifts.
+    while (index < 0)
+        index += alphabet_length;
 
-  return alphabet[index];
+    return alphabet[index];
 }
 
 int main(int argc, char *argv[]) 
 {
-  if (argc < 3) {
-    if (argc == 2)
-      fprintf(stderr, "No message given, exiting.\n");
-    else
-      fprintf(stderr, "No shift or message given, exiting.\n");
-    return EXIT_FAILURE;
-  }
-
-  int shiftc = atoi(argv[1]);
-  if (!shiftc) { // 0 is false.
-    // Allow for '0' or similarly "-0", "+0", etc.
-    if (argv[1][0] != '0' && argv[1][1] != '0') {
-      fprintf(stderr, "Couldn't convert \"%s\" to an integer, exiting.\n", argv[1]);
-      return EXIT_FAILURE;
+    if (argc < 3) {
+        if (argc == 2)
+            fprintf(stderr, "No message given, exiting.\n");
+        else
+            fprintf(stderr, "No shift or message given, exiting.\n");
+        return EXIT_FAILURE;
     }
-    // We'll let the user shift the message 0 places if they want to.
-  }
 
-  // Run `shift()` on every character in every argument after shiftc
-  for (int i = 2; i < argc; i++) {
-    for (int j = 0; j < strlen(argv[i]); j++) {
-      printf("%c", shift(argv[i][j], shiftc));
+    int shiftc = atoi(argv[1]);
+    if (!shiftc) { // 0 is false.
+        // Allow for '0' or similarly "-0", "+0", etc.
+        if (argv[1][0] != '0' && argv[1][1] != '0') {
+            fprintf(stderr, "Couldn't convert \"%s\" to an integer, exiting.\n",
+                    argv[1]);
+            return EXIT_FAILURE;
+        }
+        // We'll let the user shift the message 0 places if they want to.
     }
-  }
-  printf("\n");
 
-  return EXIT_SUCCESS;
+    // Run `shift()` on every character in every argument after shiftc
+    for (int i = 2; i < argc; i++) {
+        for (int j = 0; j < strlen(argv[i]); j++) {
+            printf("%c", shift(argv[i][j], shiftc));
+        }
+    }
+    printf("\n");
+
+    return EXIT_SUCCESS;
 }
--- code/c/selection_sort/src/main.c
+++ code/c/selection_sort/src/main.c
@@ -3,45 +3,39 @@
 
 int main()
 {
-  int array[16] = {5, -8654, 2, 23, 435, 23, 23465, 4, 66765, 0, 3534, 9,  32, 76, -92, 10};
-  int pos_i, // Index currently being sorted
-      i, // Position in unsorted subarray
-      pos_min, // Position of currently known smallest number
-      temp; // Temporarily holds an item during swapping
+    int array[16] = {5, -8654, 2, 23, 435, 23, 23465, 4, 66765, 0, 3534, 9,  32,
+      76, -92, 10};
+    int pos_i, // Index currently being sorted
+        i, // Position in unsorted subarray
+        pos_min, // Position of currently known smallest number
+        temp; // Temporarily holds an item during swapping
 
-  // Figure out how many items are in `array`.
-  // We could just use 16 here, but now you know how to do this if you didn't
-  // already :).
-  int arrsize = sizeof array / sizeof array[0];
+    // Yes yes we could just use 16 but this is more resilient
+    int arrsize = sizeof array / sizeof array[0];
 
-  for (pos_i = 0; pos_i < arrsize; pos_i++)
-  {
-    pos_min = pos_i;
-    for (i = pos_i + 1; i < arrsize; i++)
-    {
-      if (array[i] < array[pos_min])
-        pos_min = i;
+    for (pos_i = 0; pos_i < arrsize; pos_i++) {
+        pos_min = pos_i;
+        for (i = pos_i + 1; i < arrsize; i++) {
+            if (array[i] < array[pos_min])
+              pos_min = i;
+        }
+
+        // Swap only if necessary
+        if (pos_min != pos_i) {
+            temp = array[pos_min];
+            array[pos_min] = array[pos_i];
+            array[pos_i] = temp;
+        }
     }
 
-    // Swap only if necessary
-    if (pos_min != pos_i)
-    {
-      temp = array[pos_min];
-      array[pos_min] = array[pos_i];
-      array[pos_i] = temp;
+    printf("[");
+    for (int j = 0; j < arrsize; j++) {
+        if (j < arrsize - 1)
+            printf("%i, ", array[j]);
+        else
+            printf("%i", array[j]);
     }
-  }
+    printf("]\n");
 
-  printf("[");
-  for (int j = 0; j < arrsize; j++)
-  {
-    if (j < arrsize - 1)
-      printf("%i, ", array[j]);
-    // Don't print a trailing ", " for the last item in the array
-    else
-      printf("%i", array[j]);
-  }
-  printf("]\n");
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
