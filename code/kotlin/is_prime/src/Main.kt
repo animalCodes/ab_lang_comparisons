4194325d104a9f03dc8ac74586e5f25374226e67
animalCodes <84966313+animalCodes@users.noreply.github.com>
2024-09-03 17:32:13 +0100

    Clean up kotlin is_prime implementation

--- code/kotlin/is_prime/src/Main.kt
+++ code/kotlin/is_prime/src/Main.kt
@@ -1,18 +1,5 @@
 package comparisons.kotlin.isprime
 
-/**
- * A recursive implementation of trial division
- * @param input Number we are trying to figure out if prime
- * @param counter Counter variable, primarily for recursion
- * @return Whether `input` is prime
- */
-tailrec fun recursiveTrialDivision(input: Int, counter: Int): Boolean {
-    // Kotlin specifically has support for recursive functions, so we may as well use one.
-    if (counter*counter >= input) return true
-    else if (input % counter == 0 || input % counter+2 == 0) return false
-    else return recursiveTrialDivision(input, counter+6)
-}
-
 fun main(args: Array<String>) {
     if (args.size <= 0) {
         System.err.println("No number specified, exiting.")
@@ -21,22 +8,30 @@ fun main(args: Array<String>) {
 
     val input: Int? = args[0].toIntOrNull()
     if (input == null) {
-        System.err.println("Unable to convert \"${args[0]}\" to a number, exiting.")
+        System.err.println(
+            "Unable to convert \"${args[0]}\" to a number, exiting.")
         System.exit(2)
-        return // Return so the compiler knows we won't be accessing a null value.
+        return
     }
 
-    if (input < 2 || input > 2 && input % 2 == 0 || input > 3 && input % 3 == 0) {
+    if (input < 2
+        || input > 2 && input % 2 == 0
+        || input > 3 && input % 3 == 0) {
         println("False")
-        System.exit(0)
+        return
     }
 
-    if (recursiveTrialDivision(input, 6)) {
-        println("True")
-        System.exit(0)
-    } else {
-        println("False")
-        System.exit(0)
+    // We could use a range here to generate the sequence, but that would
+    // require calculating the square root of `input`.
+    var i = 5
+    while (i * i < input) {
+        if (input % i == 0 || input % i+2 == 0) {
+            println("False")
+            return
+        }
+        i += 6
     }
+
+    println("True")
 }
 
