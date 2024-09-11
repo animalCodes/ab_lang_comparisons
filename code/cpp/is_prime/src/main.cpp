5815ed5a007e0a2d69f137874c6580f1b9815ad8
animalCodes <84966313+animalCodes@users.noreply.github.com>
2024-09-11 14:10:18 +0100

    Update C++ is_prime implementation to match spec

--- code/cpp/is_prime/src/main.cpp
+++ code/cpp/is_prime/src/main.cpp
@@ -1,25 +1,33 @@
+#include <cstdlib>
 #include <iostream>
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        cerr << "No argument specified, exiting.\n";
+        cerr << "No number specified, exiting.\n";
         return 1;
     }
 
-    int num = atoi(argv[1]);
-    if (num == 0 && *argv[1] != '0') {
-        cerr << "Unable to convert \""
-             << argv[1] 
-             << "\" to a number, exiting.\n";
+    char *end;
+    int num = strtol(argv[1], &end, 10);
+
+    if (end == argv[1]) {
+        cerr << '"' << argv[1] << '"'
+             << " cannot be converted into a number, exiting.\n";
         return 2;
+    }
+
+    if (*end == '.') {
+        cout << "False\n";
+        return 0;
     }
 
     if (num < 2 || num > 2 && num % 2 == 0 || num > 3 && num % 3 == 0) {
         cout << "False\n";
         return 0;
     }
-    
+
     for (int divisor = 5; divisor*divisor < num; divisor += 6) {
         if (num % divisor == 0 || num % (divisor+2) == 0) {
             cout << "False\n";
