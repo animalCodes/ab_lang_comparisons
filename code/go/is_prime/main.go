2f1e2dc39b0648fd73ceb6dc9ef1c3eea07df1fd
animalCodes <84966313+animalCodes@users.noreply.github.com>
2024-09-11 16:00:48 +0100

    Update Go is_prime implementation to match spec

--- code/go/is_prime/main.go
+++ code/go/is_prime/main.go
@@ -2,29 +2,39 @@ package main
 
 import (
     "fmt"
+    "math"
     "os"
     "strconv"
 )
 
 func main() {
     if len(os.Args) < 2 {
-        fmt.Fprintln(os.Stderr, "No number specified, exiting")
+        fmt.Fprintln(os.Stderr, "No number specified, exiting.")
         os.Exit(1)
     }
 
-    num, err := strconv.Atoi(os.Args[1])
+    n, err := strconv.ParseFloat(os.Args[1], 64)
     if err != nil {
-        fmt.Fprintf(os.Stderr, "Unable to convert %s to an int, exiting.\n", os.Args[1]);
-        os.Exit(2)
+        fmt.Fprintf(os.Stderr,
+            "\"%s\" cannot be converted into a number, exiting.\n", os.Args[1])
+        os.Exit(1)
     }
 
-    if num < 2 || num > 2 && num % 2 == 0 || num > 3 && num % 3 == 0 {
+    if (!(n == math.Floor(n))) {
         fmt.Println("False")
         os.Exit(0)
     }
 
-    for i := 5; i*i < num; i += 6 {
-        if num % i == 0 || num % i+2 == 0 {
+    // The remainder operator (%) is only defined for integers.. Because reasons
+    var m = int(n)
+
+    if m < 2 || m > 2 && m % 2 == 0 || m > 3 && m % 3 == 0 {
+        fmt.Println("False")
+        os.Exit(0)
+    }
+
+    for i := 5; i*i < m; i += 6 {
+        if m % i == 0 || m % i+2 == 0 {
             fmt.Println("False")
             os.Exit(0)
         }
