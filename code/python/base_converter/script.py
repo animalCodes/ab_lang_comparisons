6b6f531ff959b5a9372eb773cee3d211d048e8c0
animalCodes <84966313+animalCodes@users.noreply.github.com>
2025-02-18 13:58:24 +0000

    Implement base_converter in Python

---
+++ code/python/base_converter/script.py
@@ -0,0 +1,27 @@
+import sys
+
+# Python base_converter
+
+if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
+        print("No base or number specified, exiting.", file=sys.stderr)
+    else:
+        print("No number specified, exiting.", file=sys.stderr)
+    sys.exit(1)
+
+if not sys.argv[2].isdecimal():
+    print(f"\"{sys.argv[2]}\" isn't a decimal integer. exiting.")
+    sys.exit(1)
+
+n = int(sys.argv[2])
+
+match sys.argv[1]:
+    case "2":
+        print(f"{n:b}")
+    case "8":
+        print(f"{n:o}")
+    case "16":
+        print(f"{n:X}")
+    case _:
+        print("Base must be 2, 8, or 16, exiting.", file=sys.stderr)
+        sys.exit(1)
